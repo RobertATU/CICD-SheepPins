e2c12925940d654dd75df16733fedeb3fa0cb64b
Robert <g00395612@atu.ie>
2023-12-21 13:36:20 +0000

    Initial Commit

---
+++ src/main/java/ie/atu/cicdsheeppins/CicdSheepPinsApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdsheeppins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSheepPinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSheepPinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/cicdsheeppins/CicdSheepPinsApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdsheeppins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSheepPinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
